de98d5e2af570a4a59d3548f516a2e51a240d07d
fabiogeneral <fabiogeneral@gmail.com>
2015-10-09 11:35:07 -0300

    Initial Commit

---
+++ To-do/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  To-do
+//
+//  Created by Fabio General on 10/9/15.
+//  Copyright © 2015 Fabio General. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ To-do/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  To-do
+//
+//  Created by Fabio General on 10/9/15.
+//  Copyright © 2015 Fabio General. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
